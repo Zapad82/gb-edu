5da8da2d0918baa931ddf76a7ae469e9b6793952
Zapad82 <strana39@gmail.com>
2023-07-15 18:29:09 +0500

    Лекция 1. Семинар 1. Основы js. ДЗ 1.

---
+++ Основы JS/Seminar1/hw/task2.js
@@ -0,0 +1,3 @@
+const name = "Наиль";
+const admin = name;
+console.log(admin);
---
+++ Основы JS/Seminar1/work_1/index.js
@@ -0,0 +1,51 @@
+// console.log('Привет из index.js');
+
+/* const num = 123;
+console.log(num);
+let a;
+a = 5;
+a = 7;
+console.log(a); */
+
+
+// let b;
+// b = 1 + 2 + 3;
+// console.log(b);
+
+
+// let [x, y] = [1, 2];
+// console.log(x, y);
+
+
+// const c = 10;
+// const d = 2;
+// const sum = c + d;
+// const sub = c - d;
+// const mult = c * d;
+// const div = c / d;
+// const res = sum + sub + mult + div;
+// console.log('Результат суммы всех переменных равен: ' + res);
+// console.log(`
+//              Сумма = ${sum}
+//              Разность = ${sub}
+//              Умножение = ${mult}
+//              Деление = ${div}
+//             `);
+
+
+// const a = 1.5;
+// const b = 0.75;
+// const c = a + b;
+// console.log(c);
+
+// const a = 5;
+// const b = 5;
+// console.log(-b);
+
+const name = 'Nail';
+console.log(name);
+
+const a = 'Java';
+const b = 'Script';
+console.log(a + ' ' + b);
+console.log(`${a} ${b}`);
